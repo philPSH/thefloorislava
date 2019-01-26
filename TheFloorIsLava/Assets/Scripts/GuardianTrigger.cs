54f56b1c868299d7f46434eac4bdfa983495ee38
JanekUchman <janekuchman@hotmail.co.uk>
2019-01-26 20:29:53 +0000

    Adding in the parent scene, questions, floating text, fonts, triggers, etc.

---
+++ TheFloorIsLava/Assets/Scripts/GuardianTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianTrigger : MonoBehaviour {
+    public QuestionManager questionManager;
+    public QuestionManager.Guardians guardian;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            questionManager.EnteredTrigger(guardian);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            questionManager.ExitTrigger();
+        }
+    }
+}
---
+++ TheFloorIsLava/Assets/Scripts/MoveUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour {
+    public float speed;
+    private RectTransform rect;
+	// Use this for initialization
+	void Start () {
+        rect = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rect.position = new Vector3(rect.position.x, rect.position.y + speed, rect.position.z);
+	}
+}
