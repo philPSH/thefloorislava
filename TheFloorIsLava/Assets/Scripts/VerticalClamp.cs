8f83f84c02f3453315d6baa74b8c926983dbeb6f
Andrew Murdoch <1500407@uad.ac.uk>
2019-01-27 14:11:04 +0000

    Added camera tracking script

---
+++ TheFloorIsLava/Assets/Scripts/VerticalClamp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalClamp : MonoBehaviour {
+
+    private float upperLimit = 1.8f;
+    private float lowerLimit = -2f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if(transform.parent.transform.position.y > upperLimit)
+        {
+            transform.position = new Vector3(transform.parent.transform.position.x, upperLimit, -8f);
+        }
+        else if(transform.parent.transform.position.y < lowerLimit)
+        {
+            transform.position = new Vector3(transform.parent.transform.position.x, lowerLimit, -8f);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.parent.transform.position.x, transform.parent.transform.position.y, -8f);
+        }
+
+        Debug.Log(transform.position);
+	}
+}
